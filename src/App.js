5fdd89022654b49e1d73d5593ce63fd8433b91ff
zenyattasindc <zenyattasindc@gmail.com>
2025-04-21 12:25:48 +0200

    testing token

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn React - and love it
         </a>
       </header>
     </div>
